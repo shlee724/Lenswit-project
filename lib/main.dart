36d62e04714d087e625417ea3d0ce44bb437a01e
paperman1 <powerplant724@gmail.com>
2024-09-23 14:55:51 +0900

    Added Start Button On The Image

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,27 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset('assets/images/main_page.png', fit: BoxFit.cover,),  // 이미지 표시
+      body: Stack(
+          children: <Widget>[
+          // 배경 이미지
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/main_page.png',
+              fit: BoxFit.cover, // 이미지가 화면을 채우도록 설정
+            ),
+          ),
+          // 이미지 위에 버튼
+          Positioned(
+            bottom: 50, // 버튼의 위치를 이미지 위에 설정
+            left: 50,   // 버튼의 위치 설정
+            child: ElevatedButton(
+              onPressed: () {
+                print("버튼 클릭됨");
+              },
+              child: Text('시작하기'),
+            ),
+          ),
+        ],
       ),
     );
   }
